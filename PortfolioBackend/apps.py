3dfa38bd08574d38d194baed487c40346f4d49c5
Software Developer <yinyinbei0717@gmail.com>
2024-12-28 06:54:53 +0900

    image loading success

--- PortfolioBackend/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PortfoliobackendConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PortfolioBackend'
--- system/settings.py
+++ system/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'PortfolioBackend',
+    'Portfolio',
     'corsheaders',
     'SecurityReinvent.SR_Base',
     'SecurityReinvent.visitor',
