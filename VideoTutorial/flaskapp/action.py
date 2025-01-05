3ce304c338ca13aa4e256395301834399789feb1
Software Developer <yinyinbei0717@gmail.com>
2025-01-05 21:41:28 +0900

    VideoTutorial page rendering

--- VideoTutorial/flaskapp/action.py
+++ VideoTutorial/flaskapp/action.py
@@ -9,7 +9,7 @@ bp = Blueprint('action', __name__)
 def index():
     return render_template('index.html')
 
-@bp.route('/submit', methods=('POST',))
+@bp.route('/submit', methods=['POST'])
 def submit():
     try:
         product = request.form.get('product')
@@ -39,101 +39,101 @@ def home():
 
 
 # top loader template render
-@bp.route('/topnoise', methods=('POST',))
+@bp.route('/topnoise', methods=['POST'])
 def topnoise():
     return render_template('tl/noise.html') # do something
 
-@bp.route('/topcloth', methods=('POST',))
+@bp.route('/topcloth', methods=['POST'])
 def topcloth():  
     return render_template('tl/clothing.html') # do something
 
-@bp.route('/topcycle', methods=('POST',))
+@bp.route('/topcycle', methods=['POST'])
 def topcycle():         
     return render_template('tl/cycle.html') # do something
 
-@bp.route('/topleak', methods=('POST',))
+@bp.route('/topleak', methods=['POST'])
 def topleak():  
     return render_template('tl/leak.html') # do something
 
-@bp.route('/toperror', methods=('POST',))
+@bp.route('/toperror', methods=['POST'])
 def toperror():  
     return render_template('tl/error.html') # do something'
 
-@bp.route('/topfill', methods=('POST',))
+@bp.route('/topfill', methods=['POST'])
 def topfill():  
     return render_template('tl/filling.html') # do something
 
-@bp.route('/topdrain', methods=('POST',))
+@bp.route('/topdrain', methods=['POST'])
 def topdrain():  
     return render_template('tl/drainage.html') # do something
 
-@bp.route('/topdispense', methods=('POST',))
+@bp.route('/topdispense', methods=['POST'])
 def topdispense():  
     return render_template('tl/dispenser.html') # do something
 
-@bp.route('/topdoor', methods=('POST',))
+@bp.route('/topdoor', methods=['POST'])
 def topdoor():  
     return render_template('tl/door.html') # do something
 
-@bp.route('/toppower', methods=('POST',))
+@bp.route('/toppower', methods=['POST'])
 def toppower():  
     return render_template('tl/power.html') # do something
 
-@bp.route('/topsmell', methods=('POST',))
+@bp.route('/topsmell', methods=['POST'])
 def topsmell():  
     return render_template('tl/smell.html') # do something
 
-@bp.route('/topthinq', methods=('POST',))
+@bp.route('/topthinq', methods=['POST'])
 def topthinq():      
     return render_template('tl/thinq.html') # do something
 
 
 #front loader template render
-@bp.route('/frontnoise', methods=('POST',))
+@bp.route('/frontnoise', methods=['POST'])
 def frontnoise():
     return render_template('fl/noise.html') # do something
 
-@bp.route('/frontcloth', methods=('POST',))
+@bp.route('/frontcloth', methods=['POST'])
 def frontcloth():   
     return render_template('fl/clothing.html') # do something
 
-@bp.route('/frontcycle', methods=('POST',))
+@bp.route('/frontcycle', methods=['POST'])
 def frontcycle():         
     return render_template('fl/cycle.html') # do something
 
-@bp.route('/fronfleak', methods=('POST',))
-def fronfleak():  
+@bp.route('/frontleak', methods=['POST'])
+def frontleak():  
     return render_template('fl/leak.html') # do something
 
-@bp.route('/fronterror', methods=('POST',))
+@bp.route('/fronterror', methods=['POST'])
 def fronterror():  
     return render_template('fl/error.html') # do something'
 
-@bp.route('/frontfill', methods=('POST',))
+@bp.route('/frontfill', methods=['POST'])
 def frontfill():  
     return render_template('fl/filling.html') # do something
 
-@bp.route('/frontdrain', methods=('POST',))
+@bp.route('/frontdrain', methods=['POST'])
 def frontdrain():  
     return render_template('fl/drainage.html') # do something
 
-@bp.route('/frontdispense', methods=('POST',))
+@bp.route('/frontdispense', methods=['POST'])
 def frontdispense():  
     return render_template('fl/dispenser.html') # do something
 
-@bp.route('/frontdoor', methods=('POST',))
+@bp.route('/frontdoor', methods=['POST'])
 def frontdoor():  
     return render_template('fl/door.html') # do something
 
-@bp.route('/frontpower', methods=('POST',))
+@bp.route('/frontpower', methods=['POST'])
 def frontpower():  
     return render_template('fl/power.html') # do something
 
-@bp.route('/frontsmell', methods=('POST',))
+@bp.route('/frontsmell', methods=['POST'])
 def frontsmell():  
     return render_template('fl/smell.html') # do something
 
-@bp.route('/frontthinq', methods=('POST',))
+@bp.route('/frontthinq', methods=['POST'])
 def frontthinq():      
     return render_template('fl/thinq.html') # do something
 
