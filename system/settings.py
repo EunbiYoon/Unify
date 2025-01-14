32b84e9237483dbc30f88c5a26586307def87764
Software Developer <yinyinbei0717@gmail.com>
2025-01-14 15:10:57 +0900

    need to add without http in allowed_host

--- system/settings.py
+++ system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2y9yq)pfjh9n67ydaeduxr4ma6)_9ms%78$kskv_xpm=u_4q37
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["unify-production-e96d.up.railway.app","https://yooneunbi.com","localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["unify-production-e96d.up.railway.app","yooneunbi.com","localhost","127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://unify-production-e96d.up.railway.app",
