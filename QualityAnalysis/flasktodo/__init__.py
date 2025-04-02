9c5ddc50b53675eb23ca0d6890aa8ed4827aba30
EunbiYoon1 <bianca.yoon@cheilpengtai.com>
2025-04-02 15:07:28 +0900

    change wsgi, manage, init

--- QualityAnalysis/flasktodo/__init__.py
+++ QualityAnalysis/flasktodo/__init__.py
@@ -1,12 +1,7 @@
-
 import os
-
-from flask import Flask, render_template
-# from QualityAnalysis.flasktodo import todo
-# from QualityAnalysis.flasktodo.todo import bp
-from flasktodo import todo
-from flasktodo.todo import bp
 import requests
+from flask import Flask, render_template
+from .todo import bp  # ✅ 상대경로 import (배포 시 필수)
 
 
 def handle_401(e):
@@ -18,14 +13,16 @@ def handle_404(e):
 def handle_500(e):
     return render_template('error/500.html'), 500
 
-def create_app():
-    todo = Flask(__name__, instance_relative_config=True)
 
-    todo.register_error_handler(401, handle_401)
-    todo.register_error_handler(404, handle_404)
-    todo.register_error_handler(500, handle_500)
-    
-    #### Flask URL 선언 ####
-    todo.register_blueprint(bp,url_prefix='/qualityanalysisdashboard')
-    
-    return todo
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+
+    # 에러 핸들러 등록
+    app.register_error_handler(401, handle_401)
+    app.register_error_handler(404, handle_404)
+    app.register_error_handler(500, handle_500)
+
+    # 블루프린트 등록
+    app.register_blueprint(bp, url_prefix='/qualityanalysisdashboard')
+
+    return app
