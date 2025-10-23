f1bb7b74699eee60f5231ff8e8b8da34ac53909e
Eunbi Yoon <eunbi@Eunbiui-MacBookPro.local>
2025-10-23 18:07:43 +0900

    wet

--- system/settings.py
+++ system/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["wet-cristy-eunbiyoon071717-3687c184.koyeb.app","www.yooneunbi.com","yooneunbi.com","localhost","127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "wet-cristy-eunbiyoon071717-3687c184.koyeb.app/",
+    "wet-cristy-eunbiyoon071717-3687c184.koyeb.app",
     "www.yooneunbi.com",
     "yooneunbi.com"
 ]
